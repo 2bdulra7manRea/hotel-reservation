ae5bf7b5df64d224ffa617c070d24669629ce126
Abdelrahman <abdelrahman.mosa99@gmail.com>
2021-03-28 18:20:05 +0200

    update two

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 
 import { EatLoading } from 'react-loadingg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'font-awesome/css/font-awesome.min.css'
+
 import { Provider } from 'react-redux';
 import store from './store/store';
 ReactDOM.render(
